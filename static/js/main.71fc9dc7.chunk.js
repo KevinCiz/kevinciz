a20b60f456e65e7c591255b95175f0de61e79850
KevinCiz <lauxianlun@gmail.com>
2020-11-09 20:16:34 +0800

    Updates

---
+++ static/js/main.71fc9dc7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpkevinciz=this.webpackJsonpkevinciz||[]).push([[0],{10:function(e,n,c){},11:function(e,n,c){"use strict";c.r(n);var t=c(0),i=c(1),r=c.n(i),s=c(3),o=c.n(s);c(9),c(10);var a=function(){return Object(t.jsx)("div",{className:"App",children:Object(t.jsx)("header",{className:"App-header",children:Object(t.jsx)("h1",{children:"Kevin's Portfolio"})})})};o.a.render(Object(t.jsx)(r.a.StrictMode,{children:Object(t.jsx)(a,{})}),document.getElementById("root"))},9:function(e,n,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.71fc9dc7.chunk.js.map
